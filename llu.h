8447ac04b9978ccf15f8ee65deec61b2bb5fb7ea
Nikos Plugachev <nikos@plugachev.com>
2022-11-18 22:11:16 -0500

    Added resource iteration

--- llu.h
+++ llu.h
@@ -8,6 +8,36 @@
 #include <sys/time.h>
 #include <stdio.h>
 
+extern void* (*llu_realloc)(void* alloc, size_t oldSize, size_t newSize);
+void* llu_malloc(size_t size);
+void* llu_calloc(int cnt, size_t size);
+void llu_free(void* alloc, size_t size);
+
+#define llu_dynarr(type) (type*)((int*)llu_calloc(2, sizeof(int)) + 2)
+
+#define llu_dynarrPush(arrPtr, val) do { \
+        void* arr = (void*)(*(arrPtr)); \
+        int* ptr = (int*)arr - 2; \
+        ptr[0]++; \
+        if(ptr[0] > ptr[1]) { \
+            size_t oldSize = ptr[1] * sizeof(**(arrPtr)) + 2 * sizeof(int); \
+            ptr[1] = ptr[1] == 0 ? 8 : 2 * ptr[1]; \
+            size_t newSize = ptr[1] * sizeof(**(arrPtr)) + 2 * sizeof(int); \
+            *(arrPtr) = (void*)llu_realloc(ptr, oldSize, newSize) + 2 * sizeof(int); \
+        } \
+        arr = (void*)(*(arrPtr)); \
+        ptr = (int*)arr - 2; \
+        (*(arrPtr))[ptr[0] - 1] = val; \
+    } while(0);
+
+#define llu_freeDynarr(arrPtr) do { \
+        int* ptr = (int*)(*(arrPtr)) - 2; \
+        size_t size = ptr[1] * sizeof(**(arrPtr)) + 2 * sizeof(int); \
+        llu_free(ptr, size); \
+        (*(arrPtr)) = NULL; \
+    } while(0);
+
+
 extern double llu_timerStart[256];
 extern int llu_currTimer;
 
@@ -41,6 +71,7 @@ typedef struct {
     int slots;
     int currMagic;
     llu_resourceHeader* firstFree;
+    llu_resourceHeader* firstUsed;
 } llu_resourcePool;
 
 typedef struct {
@@ -55,8 +86,12 @@ llu_handle llu_allocResource(llu_resourcePool* pool);
 bool llu_verifyHandle(llu_handle handle);
 void* llu_getResourcePtr(llu_handle handle); 
 void llu_deallocResource(llu_handle handle);
+llu_handle llu_nullHandle(llu_resourcePool* pool);
+llu_handle llu_firstResource(llu_resourcePool* pool);
+llu_handle llu_nextResource(llu_handle handle);
 
 #define llu_stringify(...) #__VA_ARGS__
+#define llu_call(funcPtr) if((funcPtr) != NULL) (funcPtr)
 
 #define llu_arenaScope(arena) do { void* _scope_begin_ = (arena)->curr;
 #define llu_arenaScopeEnd() (arena)->curr = _scope_begin_; } while(0);
@@ -66,9 +101,29 @@ typedef struct {
     char* str;
 } llu_str;
 
-llu_str llu_makeString(const char* str);
+llu_str llu_makeString(char* str);
 
 #ifdef LLU_IMPLEMENTATION
+
+static void* llu_defaultRealloc(void* alloc, size_t oldSize, size_t newSize) {
+    return realloc(alloc, newSize);
+}
+
+void* (*llu_realloc)(void* alloc, size_t oldSize, size_t newSize) = llu_defaultRealloc;
+
+void* llu_malloc(size_t size) {
+    return llu_realloc(NULL, 0, size);
+}
+
+void* llu_calloc(int cnt, size_t size) {
+    void* alloc = llu_malloc(cnt * size); 
+    memset(alloc, 0, cnt * size);
+    return alloc;
+}
+
+void llu_free(void* alloc, size_t size) {
+    llu_realloc(alloc, size, 0);
+}
 
 double llu_timerStart[256];
 int llu_currTimer = 0;
@@ -137,6 +192,7 @@ llu_resourcePool* llu_makeResourcePool(size_t resourceSize) {
     pool->resourceSize = resourceSize;
     pool->slots = 0;
     pool->firstFree = NULL;
+    pool->firstUsed = NULL;
     return pool;
 }
 
@@ -155,7 +211,8 @@ llu_handle llu_allocResource(llu_resourcePool* pool) {
         res->slot = pool->slots;
         pool->slots++;
     }
-    res->next = NULL;
+    res->next = pool->firstUsed;
+    pool->firstUsed = res;
     res->magic = pool->currMagic;
     pool->currMagic++;
 
@@ -167,8 +224,13 @@ llu_handle llu_allocResource(llu_resourcePool* pool) {
     return handle;
 }
 
-bool llu_verifyHandle(llu_handle handle) {
+static llu_resourceHeader* llu_getHeader(llu_handle handle) {
     llu_resourceHeader* header = handle.pool->arena->base + handle.slot * (sizeof(llu_resourceHeader) + handle.pool->resourceSize);
+    return header;
+}
+
+bool llu_verifyHandle(llu_handle handle) {
+    llu_resourceHeader* header = llu_getHeader(handle); 
     return header->magic == handle.magic;
 }
 
@@ -176,7 +238,7 @@ void* llu_getResourcePtr(llu_handle handle) {
     if(!llu_verifyHandle(handle)) {
         return NULL;
     }
-    llu_resourceHeader* header = handle.pool->arena->base + handle.slot * (sizeof(llu_resourceHeader) + handle.pool->resourceSize);
+    llu_resourceHeader* header = llu_getHeader(handle);
     return (void*)header + sizeof(llu_resourceHeader);
 } 
 
@@ -184,13 +246,48 @@ void llu_deallocResource(llu_handle handle) {
     if(!llu_verifyHandle(handle)) {
         return;
     }
-    llu_resourceHeader* header = handle.pool->arena->base + handle.slot * (sizeof(llu_resourceHeader) + handle.pool->resourceSize);
+    llu_resourceHeader* header = llu_getHeader(handle); 
+    if(header == handle.pool->firstUsed) {
+        handle.pool->firstUsed = header->next;
+    }
     header->magic = -1;
     header->next = handle.pool->firstFree;
     handle.pool->firstFree = header;
 }
 
-llu_str llu_makeString(const char* str) {
+llu_handle llu_nullHandle(llu_resourcePool* pool) {
+    llu_handle handle;
+    handle.pool = pool;
+    handle.magic = -1;
+    handle.slot = 0;
+    return handle;
+}
+
+llu_handle llu_firstResource(llu_resourcePool* pool) {
+    if(pool->firstUsed == NULL)
+        return llu_nullHandle(pool);
+    llu_handle first;
+    first.pool = pool;
+    first.slot = pool->firstUsed->slot;
+    first.magic = pool->firstUsed->magic;
+    return first;
+}
+
+llu_handle llu_nextResource(llu_handle handle) {
+    llu_resourceHeader* header = llu_getHeader(handle);
+    llu_resourceHeader* nextHeader = header->next;
+    if(nextHeader == NULL)
+        return llu_nullHandle(handle.pool);
+    llu_handle nextHandle;
+    nextHandle.pool = handle.pool;
+    nextHandle.slot = nextHeader->slot;
+    nextHandle.magic = nextHeader->magic;
+    return nextHandle;
+}
+
+
+
+llu_str llu_makeString(char* str) {
     llu_str s;
     s.len = strlen(str);
     s.str = str;
